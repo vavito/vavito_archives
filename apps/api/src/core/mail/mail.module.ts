0341c35bd53497ce87facf6d33b671b8f4fa3146
vavito <joaovictor2126@gmail.com>
2026-08-24 13:46:06 -0300

    feat(mail): substitui serviço provisório pelo Resend

--- apps/api/src/core/mail/mail.module.ts
+++ apps/api/src/core/mail/mail.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Resend } from 'resend';
 
+import type { ApplicationConfig } from '@api/core/config/app.config';
+import { RESEND_EMAIL_CLIENT } from '@api/core/mail/mail.constants';
 import { MailService } from '@api/core/mail/services/mail.service';
-import { PendingMailService } from '@api/core/mail/services/pending-mail.service';
+import { ResendService } from '@api/core/mail/services/resend.service';
 
 @Module({
   exports: [MailService],
-  providers: [{ provide: MailService, useClass: PendingMailService }],
+  providers: [
+    {
+      inject: [ConfigService],
+      provide: RESEND_EMAIL_CLIENT,
+      useFactory: (configService: ConfigService<ApplicationConfig, true>) =>
+        new Resend(configService.get('resend.apiKey', { infer: true })).emails,
+    },
+    ResendService,
+    { provide: MailService, useExisting: ResendService },
+  ],
 })
 export class MailModule {}
