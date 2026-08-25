a43c14909dc6ead9a21d2f4163ca79da064cb808
vavito <joaovictor2126@gmail.com>
2026-08-25 15:09:57 -0300

    feat(newsletter): define contrato de persistência

---
+++ apps/api/src/modules/newsletter/repositories/subscribers.repository.ts
@@ -0,0 +1,9 @@
+import type { Subscriber } from '@api/modules/newsletter/domain/entities/subscriber.entity';
+
+export abstract class SubscribersRepository {
+  abstract createIfEmailAvailable(subscriber: Subscriber): Promise<boolean>;
+  abstract findByConfirmationTokenHash(tokenHash: string): Promise<Subscriber | null>;
+  abstract findByEmail(email: string): Promise<Subscriber | null>;
+  abstract findByUnsubscribeTokenHash(tokenHash: string): Promise<Subscriber | null>;
+  abstract save(subscriber: Subscriber): Promise<void>;
+}
