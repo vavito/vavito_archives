14d98bcb2fdfffe86c0f89ca62faead933d218c8
vavito <joaovictor2126@gmail.com>
2026-08-26 08:54:50 -0300

    feat(contact): registra módulo de contato na API

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -8,6 +8,7 @@ import configuration, { environmentFilePaths } from '@api/core/config/configurat
 import { validateEnvironment } from '@api/core/config/env.validation';
 import { PrismaModule } from '@api/core/database/prisma.module';
 import { CommentsModule } from '@api/modules/comments/comments.module';
+import { ContactModule } from '@api/modules/contact/contact.module';
 import { EngagementModule } from '@api/modules/engagement/engagement.module';
 import { HealthModule } from '@api/modules/health/health.module';
 import { MediaModule } from '@api/modules/media/media.module';
@@ -27,6 +28,7 @@ import { ProfilesModule } from '@api/modules/profiles/profiles.module';
     PrismaModule,
     AuthModule,
     CommentsModule,
+    ContactModule,
     EngagementModule,
     HealthModule,
     MediaModule,
