ba17392718e855d68ed39294fc670bc5a378cfcb
vavito <joaovictor2126@gmail.com>
2026-08-27 22:14:27 -0300

    docs(comments): documenta filtros e operações de comentários

--- apps/api/src/modules/comments/dto/request/update-comment.dto.ts
+++ apps/api/src/modules/comments/dto/request/update-comment.dto.ts
@@ -4,7 +4,10 @@ import { ApiProperty } from '@nestjs/swagger';
 import { MAX_COMMENT_CONTENT_LENGTH } from '@api/modules/comments/domain/value-objects/comment-content.value-object';
 
 export class UpdateCommentDto {
-  @ApiProperty({ maxLength: MAX_COMMENT_CONTENT_LENGTH })
+  @ApiProperty({
+    example: 'Comentário atualizado com uma informação complementar.',
+    maxLength: MAX_COMMENT_CONTENT_LENGTH,
+  })
   @IsString()
   @MinLength(1)
   @MaxLength(MAX_COMMENT_CONTENT_LENGTH)
